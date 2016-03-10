174affd4316f83525cc47043ffee202e48b2655c
Steven Woodrum <stevenwoodrum@users.noreply.github.com>
2016-03-09 19:56:47 -0500

    Content and style

--- myScript.js
+++ myScript.js
@@ -6,9 +6,12 @@ function loadItem(index) {
 
 		activeItem = combat_robotics;
 	}
-	else {
-		activeItem = item1;
+	else if (index == 1) {
 	}
+	else if (index == 2) {
+		activeItem = board_game_AI;
+	}
+	else {}
 
 	// Update fields of HTML page
 	document.getElementById("myModalLabel").innerHTML = activeItem.title;
@@ -25,9 +28,9 @@ var combat_robotics = {
 };
 
 
-var item1 = {
-	title: "Test title",
-	date: "Test date",
-	skills: "Test skills",
-	description: "Test description"
+var board_game_AI = {
+	title: "Artificially Intelligent Board Game Player",
+	date: "Summer 2015",
+	skills: "Java, Eclipse",
+	description: "The Artificial Intelligence course (CS 4710) pitted student-submitted software agents against each other in a computer version of the Ticket to Ride(R) board game. Initial attempts at a depth-limited classical-search approach produced mediocre results. Just days before the competition, the class learned about Markov chains and the use of Markov decision processes (MDP). I scrambled to implement and test a basic MDP to compete, and ultimately won the class tournament. The Markovian AI uses a small state space of 8 carefully chosen states. It runs blazingly fast, using hashmaps for probabilistic lookups, a variant of Dijkstra's shortest-path algorithm, and several linear processes with small input sizes. The program couples short-term and long-term planning, modifying its behavior over time to maximize the overall chance of success."
 };
